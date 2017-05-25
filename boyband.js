7fd9596b310ca84f2c10383e82b1178fccac1795
Arwa <arwa.it15@gmail.com>
2017-05-25 14:40:32 -0500

    first

---
+++ boyband.js
@@ -0,0 +1,38 @@
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+function print(message)
+{
+	document.write(message);
+}
+// The number of loops to perform (what if the array changes?)
+var loopCount = bands.length;
+
+// Keep track of which band we're on in the loop
+var currentBand = "";
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-band");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+  
+  // Add the band names into the correct <div>
+  currentBand = bands[loopTracker];
+  currentVeggie = vegetables[loopTracker];
+  
+var para = document.createElement("P");                       
+var text = document.createTextNode(currentBand);      
+para.appendChild(text);                                        
+document.getElementById("boy-bands").appendChild(para);      
+
+var para2 = document.createElement("P");                       
+var text2 = document.createTextNode(currentVeggie);      
+para2.appendChild(text2);                                        
+document.getElementById("vegetables").appendChild(para2);      
+}
